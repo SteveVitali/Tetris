b5d34034a823298338c7b10c995b1c771f8e6e5a
Steve Vitali <14stevevitali@gmail.com>
2014-12-05 22:04:47 -0500

    Update high scores view to use a TableModel class generated by the HighScoresModel

--- HighScoresModel.java
+++ HighScoresModel.java
@@ -5,14 +5,51 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Map.Entry;
 
+import javax.swing.table.DefaultTableModel;
+
 public class HighScoresModel {
 
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-
     private ArrayList<Entry<String,Long>> scores;
 
     public HighScoresModel() {
         scores = new ArrayList<Entry<String,Long>>();
+    }
+
+    @SuppressWarnings("serial")
+    public DefaultTableModel getTableModel() {
+        DefaultTableModel tableModel = new DefaultTableModel() {
+            @SuppressWarnings({ "unchecked", "rawtypes" })
+            public Class getColumnClass(int column) {
+                int row = 0;
+                Object o = getValueAt(row, column);
+                if (o == null) {
+                    return Object.class;
+                } else {
+                    return o.getClass();
+                }
+            }
+        };
+        tableModel.addColumn("Name", getNames());
+        tableModel.addColumn("Time", getTimes());
+        return tableModel;
+    }
+
+    public String[] getNames() {
+        String[] names = new String[scores.size()];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = scores.get(i).getKey();
+        }
+        return names;
+    }
+
+    public String[] getTimes() {
+        String[] times = new String[scores.size()];
+        for (int i = 0; i < times.length; i++) {
+            long time = scores.get(i).getValue();
+            times[i] = TimerModel.getTimeString(time);
+        }
+        return times;
     }
 
     public void addScore(String name, long time) {
@@ -24,6 +61,7 @@ public class HighScoresModel {
         return new AbstractMap.SimpleEntry<String,Long>(name, time);
     }
 
+    // This is old code from when we weren't using a table model
     public String[][] getNamesAndScoresArray() {
         String[][] data = new String[scores.size()][2];
         for (int i = 0; i < scores.size(); i++) {
--- HighScoresView.java
+++ HighScoresView.java
@@ -1,10 +1,19 @@
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Insets;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableCellRenderer;
 
 @SuppressWarnings("serial")
 public class HighScoresView extends JPanel {
@@ -12,12 +21,9 @@ public class HighScoresView extends JPanel {
     private HighScoresModel model;
     private JTable scoresTable;
 
-    private String[] columnNames = {"User", "Score"};
-
     public HighScoresView(HighScoresModel model) {
         this.model = model;
-
-        setBackground(Color.black);
+        setBackground(AppController.BG_COLOR);
 
         model.addPropertyChangeListener(new PropertyChangeListener() {
             @Override
@@ -27,15 +33,43 @@ public class HighScoresView extends JPanel {
                 }
             }
         });
-    }
 
-    public void refreshTable() {
-        scoresTable = new JTable(model.getNamesAndScoresArray(), columnNames);
+        scoresTable = new JTable(model.getTableModel());
+        scoresTable.setDefaultRenderer(Object.class, new ScoresTableCellRenderer());
+        scoresTable.setOpaque(false);
+        scoresTable.setRowHeight(32);
+        scoresTable.setBorder(BorderFactory.createEmptyBorder());
+        scoresTable.setShowGrid(false);
+
         JScrollPane scrollPane = new JScrollPane(scoresTable);
         scrollPane.setOpaque(false);
-        scrollPane.setBackground(Color.black);
+        scrollPane.getViewport().setOpaque(false);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+
         add(scrollPane);
-        revalidate();
-        repaint();
+    }
+    public void refreshTable() {
+        scoresTable.setModel(model.getTableModel());
+    }
+
+    class ScoresTableCellRenderer extends JLabel implements TableCellRenderer {
+
+        public ScoresTableCellRenderer() {
+            setOpaque(true);
+            setForeground(Color.white);
+            setBackground(new Color(36,36,36));
+            setFont(new Font("Helvetica", Font.PLAIN, 14));
+            EmptyBorder borderPad = new EmptyBorder(new Insets(1,4,1,4));
+            Border actualBorder = BorderFactory.createLineBorder(new Color(54,54,54),1);
+            setBorder(new CompoundBorder(actualBorder, borderPad));
+        }
+        public Component getTableCellRendererComponent(
+                            JTable table, Object value,
+                            boolean isSelected, boolean hasFocus,
+                            int row, int column)
+        {
+            setText(value.toString());
+            return this;
+        }
     }
 }
